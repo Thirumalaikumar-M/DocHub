371ab5954cdf1ec641512d4a25defa88abcd6fd6
Thirumalaikumar-M <mthirumalaitk@gmail.com>
2024-02-13 20:05:39 +0530

    Setup TextEditor using React Quill

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,11 @@
-import React from "react";
+import TextEditor from "./TextEditor";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <TextEditor />
+    </>
+  );
 }
 
 export default App;
